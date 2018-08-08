0bab8f969c62b698f0243a58e39c30d3e8460a95
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2018-08-08 10:22:15 +0530

    Set the url automatically.

--- core/static/js/options.js
+++ core/static/js/options.js
@@ -13,6 +13,7 @@
         $('.settings').classList.add('settings', 'hidden');
         $('.generate').classList.remove('hidden');
     }
+    $('input[name=url]').value = window.location.href('/options/', '').replace('#', '');
 
     $('.generate form').addEventListener('submit', function(ev) {
         ev.preventDefault();
